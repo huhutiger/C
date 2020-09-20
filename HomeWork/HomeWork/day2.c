dd4ac1081df502ad024bcf9b56f44810eed03986
huhutiger <2417012257@qq.com>
2020-09-20 19:59:06 +0800

    作业

--- HomeWork/HomeWork/day2.c
+++ HomeWork/HomeWork/day2.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include<windows.h>
 
 //1. 给定两个整形变量的值，将两个值的内容进行交换。
 
@@ -121,24 +122,24 @@
 //}
 
 
-#include <string.h>
-
-int main()
-{
-	char input[20] = {0};
-	//shutdown -s -t 60
-	//system()- 执行系统命令的
-	system("shutdown -s -t 120");
-    again:
-	printf("请注意，你的电脑在1分钟内关机，如果输入:我是猪，就取消关机\n请输入>:");
-	scanf("%s", input);
-	if(strcmp(input, "我是猪") == 0)//比较两个字符串-strcmp()
-	{
-		system("shutdown -a");
-	}
-	else
-	{
-		goto again;
-	}
-	return 0;
-}
+//#include <string.h>
+//
+//int main()
+//{
+//	char input[20] = {0};
+//	//shutdown -s -t 60
+//	//system()- 执行系统命令的
+//	system("shutdown -s -t 120");
+//    again:
+//	printf("请注意，你的电脑在1分钟内关机，如果输入:我是猪，就取消关机\n请输入>:");
+//	scanf("%s", input);
+//	if(strcmp(input, "我是猪") == 0)//比较两个字符串-strcmp()
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
